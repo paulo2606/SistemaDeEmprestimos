c4fc75981444d6c16f69b537eb6cc863a2359a02
paulo2606 <paulohpereira@outlook.com.br>
2025-10-01 21:55:12 -0300

    feat: implementado banco de dados e mapeamento do banco

--- src/Controllers/WeatherForecastController.cs
+++ src/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace LibCenter.Controllers
+namespace LibCenter.src.Controllers
 {
     [ApiController]
     [Route("[controller]")]
---
+++ src/Model/UserDataModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibCenter.src.Models
+{
+    [Table("user_data")]
+    public class UserDataModel
+    {
+        [Key]
+        [Column("id_user")]
+        public int IdUser { get; set; }
+
+        [Column("email_user")]
+        public string EmailUser { get; set; }
+
+        [Column("name_user")]
+        public string NameUser { get; set; }
+
+        [Column("phone_user")]
+        public string PhoneUser { get; set; }
+
+        [Column("registration")]
+        public string Registration { get; set; }
+
+        [Column("pwd_hash_user")]
+        public string PwdHashUser { get; set; }
+
+        [Column("dt_criation_user")]
+        public DateTime DtCriationUser { get; set; }
+    }
+}
